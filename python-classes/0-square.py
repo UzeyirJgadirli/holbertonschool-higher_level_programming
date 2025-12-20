727a66665552378fc721e48bb87551d21fcff1c7
UzeyirJgadirli <uzeyirhajigadirli@outlook.com>
2025-12-20 18:37:11 +0000

    Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Square Class
-
-This class don't do anything for the moment
-
+"""
+This module defines a Square class.
 """
 
-
 class Square:
+    """
+    Represents a square.
+    """
     pass
