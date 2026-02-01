a85520ee7efedebea8a23e42bfb3c537f1fe4da2
Uzeyir Hajigadirli <uzeyirhajigadirli@outlook.com>
2026-02-01 19:16:16 +0000

    Task 11

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const num = parseInt(process.argv[2]);
 
 function factorial (n) {
@@ -8,4 +7,5 @@ function factorial (n) {
   }
   return n * factorial(n - 1);
 }
+
 console.log(factorial(num));
